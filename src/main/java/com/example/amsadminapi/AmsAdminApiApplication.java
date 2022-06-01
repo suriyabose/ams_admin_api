986265e216f895df20c21946d11535c364007504
suriyabose <suriyakumarbose@gmail.com>
2022-06-01 16:34:09 +0530

    init_commit

---
+++ src/main/java/com/example/amsadminapi/AmsAdminApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.amsadminapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmsAdminApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmsAdminApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/amsadminapi/AmsAdminApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.amsadminapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmsAdminApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
